26d4fe230e94c5c78d8c87f524516fcdf9e2e223
Michael Mader <michaelmader@c02hmp34dv13.fmi.mairs.de>
2012-11-16 11:01:10 +0100

    made mapview scrollable inside scrollview

--- android/modules/map/src/java/ti/modules/titanium/map/TiMapView.java
+++ android/modules/map/src/java/ti/modules/titanium/map/TiMapView.java
@@ -193,6 +193,26 @@ public class TiMapView extends TiUIView
 				requestViewOnScreen = false;
 			}
 		}
+        
+        @Override
+        public boolean onTouchEvent(MotionEvent ev) {
+            int action = ev.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    // Disallow ScrollView to intercept touch events.
+                    this.getParent().requestDisallowInterceptTouchEvent(true);
+                    break;
+                    
+                case MotionEvent.ACTION_UP:
+                    // Allow ScrollView to intercept touch events.
+                    this.getParent().requestDisallowInterceptTouchEvent(false);
+                    break;
+            }
+            
+            // Handle MapView's touch events.
+            super.onTouchEvent(ev);
+            return true;
+        }
 
 		public void requestViewOnScreen(View v)
 		{
